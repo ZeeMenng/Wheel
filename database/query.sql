b800e26c0dca09bd13ddb26961e473ab7ef02c33
Zee <zeemenng@gmail.com>
2021-09-06 18:22:47 +0800

    修改国家信息表结构及数据

--- database/query.sql
+++ database/query.sql
@@ -1,4 +1,5 @@
-
+﻿/*导出数据库*/
+mysqldump -uroot -p850934828 wheel > D:\JAVA\JavaProject\wheel\database\wheel.sql
 
 /*清除字段中的空格*/
 update `gp_region_country` set `chinese_name`=replace(`chinese_name`,' ','');
